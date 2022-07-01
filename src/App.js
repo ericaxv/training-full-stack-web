5af8ef34ac1e8045fb37989a3fd53f2c1d8c8aec
Erica Xavier <ericaxav@gmail.com>
2022-06-30 21:18:53 -0300

    Commit Update App / Header / Search

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './css/index.css'
-import Header from './components/Header'
+import './css/index.css';
+import { useState } from 'react';
+import Header from './components/Header';
+import Search from './components/Search';
+
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLAH_KEY;
 
 const App = () => {
+  const [word, setWord] = useState('')
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    console.log(word);
+    fetch(`https://api.unsplash.com/photos/random?query=${word}&client_id=${UNSPLASH_KEY}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+    setWord("")
+  }
+
+
   return (
-    <div >
-      <Header title="Cadastro Centro de Custo" />
+    <div>
+      <Header title="Image Gallery" />
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
+
     </div>
+
+
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { Navbar, Container } from 'react-bootstrap';
 
+const navbarStyle = {
+  backgroundColor: 'black',
+  textAlign: 'center'
+
+};
+
 const Header = (props) => {
   const { title } = props;
   return (
-    <Navbar expand="lg" bg="dark" variant="dark">
+    <Navbar style={navbarStyle} variant="dark">
       <Container >
         <Navbar.Brand href="/">{title}</Navbar.Brand>
       </Container>
