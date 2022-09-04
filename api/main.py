b1676d65fa129b070e0cd3a0149025247a8962d4
Erica Xavier <ericaxav@gmail.com>
2022-09-04 00:05:54 -0300

    Update

--- api/main.py
+++ api/main.py
@@ -1,5 +1,5 @@
-## to install .venv run pipenv install, gitgnore will ignore all file .venv
-## comand run api = python main.py
+# to install .venv run pipenv install, gitgnore will ignore all file .venv
+# comand run api = python main.py
 import os
 import requests
 from flask import Flask, request
@@ -8,9 +8,9 @@ from flask_cors import CORS
 
 load_dotenv(dotenv_path="./.env.local")
 
-UNSPLASH_URL="https://api.unsplash.com/photos/random"
-UNSPLASH_KEY=os.environ.get("UNSPLASH_KEY", "")
-DEBUG=bool(os.environ.get("DEBUG", True))
+UNSPLASH_URL = "https://api.unsplash.com/photos/random"
+UNSPLASH_KEY = os.environ.get("UNSPLASH_KEY", "")
+DEBUG = bool(os.environ.get("DEBUG", True))
 
 if not UNSPLASH_KEY:
     raise EnvironmentError("Please create .env.local file and insert UNSPLASH_KEY")
@@ -33,7 +33,6 @@ def new_image():
     response = requests.get(url=UNSPLASH_URL, headers=headers, params=params)
     data = response.json()
     return data
-  
-
+    
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5055)
